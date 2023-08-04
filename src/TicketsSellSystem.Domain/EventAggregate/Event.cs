9c3eec7869a8f230917f23261513ab0aa30cd762
DMierzwa <dariusz.mierzwa@hotmail.com>
2023-08-04 12:55:10 +0200

    Creating an event aggragate

---
+++ src/TicketsSellSystem.Domain/EventAggregate/Event.cs
@@ -0,0 +1,23 @@
+using TicketsSellSystem.Domain.Common.Models;
+using TicketsSellSystem.Domain.EventAggregate.ValueObjects;
+
+namespace TicketsSellSystem.Domain.EventAggregate;
+
+public class Event : AggregateRoot<EventId>
+{
+    private Event(EventId eventId, EventName name)
+        : base(eventId)
+    {
+        this.Name = name;
+    }
+
+    public EventName Name { get; private set; }
+
+    public static Event Create(EventName name)
+    {
+        return
+            new Event(
+                EventId.CreateUnique(),
+                name);
+    }
+}
