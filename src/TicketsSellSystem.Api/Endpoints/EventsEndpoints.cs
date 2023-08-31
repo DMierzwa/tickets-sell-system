a2a0538d55be367255828bffa48452ef21f7464d
DMierzwa <dariusz.mierzwa@hotmail.com>
2023-08-31 20:05:34 +0200

    Creating POST request to create event

--- src/TicketsSellSystem.Api/Endpoints/EventsEndpoints.cs
+++ src/TicketsSellSystem.Api/Endpoints/EventsEndpoints.cs
@@ -1,4 +1,7 @@
-﻿namespace TicketsSellSystem.Api;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using TicketsSellSystem.Application.Events.Commands.CreateEvent;
+
+namespace TicketsSellSystem.Api;
 
 public class EventsEndpoints : IEndpoint
 {
@@ -6,8 +9,27 @@ public class EventsEndpoints : IEndpoint
     {
         var builderGroup = builder.MapGroup("/events");
 
-        builderGroup.MapGet("/", () => "GetEventEndpoint");
+        builderGroup.MapGet("/", this.GetResult);
+        builderGroup.MapPost("/", this.CreateEvent);
 
         return builderGroup;
+    }
+
+    private IResult GetResult()
+    {
+        return TypedResults.Ok("GetResult");
+    }
+
+    private async Task<IResult> CreateEvent()
+    {
+        string name = "CreateEvent name";
+        string description = "CreateEvent description";
+
+        CreateEventCommand command = new(name, description);
+        var commandHandler = new CreateEventCommandHandler();
+
+        var result = await commandHandler.Handle(command, CancellationToken.None);
+
+        return TypedResults.Ok(result);
     }
 }
